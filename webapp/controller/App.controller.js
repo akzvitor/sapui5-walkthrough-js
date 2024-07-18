143d29f2d8bac90212770bb54a28e91e4fe52faf
akzvitor <vitor_eterno@outlook.com>
2024-07-18 10:51:37 -0300

    step 36 - content density: sets different content densities for diferent devices, compact to cursor devices and cozy to touchscreen

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,12 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller"
 ], (Controller) => {
-   "use strict";
+	"use strict";
 
-   return Controller.extend("ui5.walkthrough.controller.App", {
-   });
+	return Controller.extend("ui5.walkthrough.controller.App", {
+
+		onInit() {
+			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+		}
+	});
 });
